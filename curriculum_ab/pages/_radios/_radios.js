783d4536cc2248539f3754fea1ac37a93921a8ae
htJson <myjeh@sina.com>
2018-01-10 18:59:16 +0800

    up

--- curriculum_ab/pages/_radios/_radios.js
+++ curriculum_ab/pages/_radios/_radios.js
@@ -1,7 +1,7 @@
 Page({
   data: {
     items: [
-      { name: 'USA', value: '美国' },
+      { name: 'USA', value: '美国1', size:12},
       { name: 'CHN', value: '中国', checked: 'true' },
       { name: 'BRA', value: '巴西' },
       { name: 'JPN', value: '日本' },
