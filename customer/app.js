5b6b9be9b4d191b45d10c5c2e6c19e3079f44d5b
htJson <myjeh@sina.com>
2018-05-22 19:22:09 +0800

    openid

--- customer/app.js
+++ customer/app.js
@@ -7,7 +7,7 @@ App({
     url: 'https://test-auth.aobei.com',
     dev: 'https://test-api.aobei.com/graphql',
     // dev: 'https://test-api.aobei.com/graphql',
-    timeUrl:'https://test-api.aobei.com/server/time',
+    timeUrl: 'https://test-api.aobei.com/server/time',
     code: '',
     key: 'ac928fb7-f11a-4d9f-894c-8283859bc914',
     appid: 'wx653dc689ca79ac81',
@@ -19,15 +19,15 @@ App({
   globalData: {
     isAgree: false,
     updateTokenData: '',
-    saveTokenKey: '',
     systemInfo: '',
+    tokenStorage: '',
     token: '',
-    saveTokenKey:'',
+    serverTime: 0,
+    saveTokenKey: '',
     userId: '',
     openId: '',
   },
   onLaunch: function (options) {
-    var _this = this;
     // wx.clearStorageSync()
     // 判断环境
     if (this.data.dev.indexOf('dev') != -1) {
@@ -56,7 +56,7 @@ App({
     })
     this.data.nostr = utils.randomWord(false, 32)
     this.data.scene = decodeURIComponent(options.scene)
-
+    this.getServerTime();
     // 登录
     wx.login({
       success: res => {
@@ -64,45 +64,8 @@ App({
         this.globalData.code = res.code
       }
     })
-    // setInterval(() => {
-    //   this.upadteToken()
-    // }, 10000)
-    // }, 7100000)
   },
-  getTime(res){   //根据时间判断storage是否有效
-    wx.request({
-      url: this.data.timeUrl,
-      method:'POST',
-      header:{
-        "content-type": 'application/x-www-form-urlencoded', // 默认值
-      },
-      success:timeData=>{
-        var timeNum = timeData.data.second
-        // 如果有token 缓存
-        if (res.data.token.time - timeNum < 10) {
-          // 如果token马上过期则走更新token
-          if (res.data.refresh_token.time - timeNum < 10) {
-            // 如果更新token的refresh_token过期则走游客token
-            this.touristToken()
-          } else {
-            // 如果没过期则走更新token
-            setInterval(() => {
-              this.upadteToken('c')
-            }, 10000)
-            this.globalData.updateTokenData = res.data.refresh_token.value
-            this.upadteToken()
-          }
-        } else {
-          setInterval(() => {
-            this.upadteToken('b')
-          }, 10000)
-          // 如果token还没有过期则赋值token
-          this.globalData.updateTokenData = res.data.refresh_token.value
-          this.globalData.token = res.data.token.value
-        }
-      }
-    })
-  },
+
   getOpenId(vcode) {  //获取openid     1
     wx.request({
       url: this.data.url + '/wxapi/jscode2session',
@@ -124,19 +87,53 @@ App({
         this.globalData.systemInfo.openId = res.data.openid;
         this.globalData.systemInfo = md5.hexMD5(JSON.stringify(this.globalData.systemInfo))
         this.data.openId = res.data.openid.toString();
-        wx.getStorage({  //拿取token storage
-          key: this.globalData.saveTokenKey + res.data.openid,
-          success: res=> {   //如果有storage 
-            this.getTime(res);
-          },
-          fail: res => {
-            // 如果没有缓存则走游客token
-            this.touristToken()
-          }
-        })
+        this.globalData.tokenStorage = wx.getStorageSync(this.globalData.saveTokenKey + this.globalData.openId)
+        console.log(this.globalData.tokenStorage)
+        this.judgeToken();
       }
     })
   },
+  getServerTime() {  //获取服务器时间，然后定时器增加时间
+    wx.request({
+      url: this.data.timeUrl,
+      method: 'POST',
+      header: {
+        "content-type": 'application/x-www-form-urlencoded', // 默认值
+      },
+      success: timeData => {
+        this.globalData.serverTime = timeData.data.second;
+        setInterval(() => {
+          this.globalData.serverTime += 1;
+        }, 1000)
+      }
+    })
+  },
+  judgeToken() { //判断token
+    console.log(this.globalData.serverTime, '-----------------time')
+    console.log(this.globalData.tokenStorage, '=================????????')
+    var serverTime = this.globalData.serverTime, tokenStorage = this.globalData.tokenStorage;
+    if (!this.globalData.tokenStorage) {  //如果没有缓存的token
+      this.touristToken();
+      return false;
+    }
+    // 如果有缓存的token
+    if (tokenStorage.token.time - serverTime < 10) {
+      console.log(1)
+      if (tokenStorage.refresh_token.time - serverTime < 10) {
+        console.log(2)
+        this.touristToken()
+      } else {
+        console.log(3)
+        this.globalData.updateTokenData = tokenStorage.refresh_token.value
+        this.upadteToken()
+      }
+    } else {
+      console.log(4)
+      this.globalData.updateTokenData = tokenStorage.refresh_token.value
+      this.globalData.token = tokenStorage.token.value
+    }
+  },
+
   // 游客token
   touristToken() {
     wx.request({
@@ -156,15 +153,15 @@ App({
     })
   },
 
-  setTokenStorage(res){  //更新缓存token
+  setTokenStorage(res) {  //更新缓存token
     wx.request({
-      url:this.data.timeUrl,
-      method:'POST',
-      header:{
+      url: this.data.timeUrl,
+      method: 'POST',
+      header: {
         "content-type": 'application/x-www-form-urlencoded', // 默认值
       },
-      success:timeData=>{
-        var time=timeData.data.second;
+      success: timeData => {
+        var time = timeData.data.second;
         this.globalData.token = 'Bearer ' + res.data.access_token;
         this.globalData.userId = res.data.uuid;
         this.globalData.updateTokenData = res.data.refresh_token;
@@ -172,20 +169,20 @@ App({
           key: this.globalData.saveTokenKey + this.globalData.openId,
           data: {
             "token": {
-              time: time+(2*60),
+              time: time + (2 * 60),
               value: 'Bearer ' + res.data.access_token
             },
             "refresh_token": {
-              time: time+(4*60),
+              time: time + (4 * 60),
               value: res.data.refresh_token
             }
           }
         })
       }
     })
-    
   },
-  upadteToken(key) {
+
+  upadteToken(key) {  //token 更新方法
     wx.request({
       url: this.data.url + '/oauth/token?d=' + new Date().getTime(), //仅为示例，并非真实的接口地址
       method: "POST",
@@ -203,7 +200,7 @@ App({
         'device': this.data.device
       },
       success: res => {
-        if(res.data.error == 'invalid_token'){
+        if (res.data.error == 'invalid_token') {  //如果更新出错，说明refresh_token 失效，登录失效走游客
           this.touristToken()
           return false;
         }
@@ -211,26 +208,7 @@ App({
       }
     })
   },
-  // 获取token
-  getToken() {
-    wx.request({
-      url: this.data.url + '/oauth/token',
-      method: "POST",
-      data: {
-        grant_type: 'wxm_code',
-        appid: this.data.appid,
-        code: this.globalData.code,
-      },
-      header: {
-        "content-type": 'application/x-www-form-urlencoded', // 默认值
-        "Authorization": 'Basic d3hfbV9jdXN0b206NHg5MWI3NGUtM2I3YS1iYjZ4LWJ0djktcXpjaW83ams2Zzdm',
-        "Duuid": this.globalData.systemInfo
-      },
-      success: res => {
-        this.setTokenStorage(res)
-      }
-    })
-  },
+
   // 获得幂等code
   getmstCode(fn) {
     wx.request({
@@ -255,54 +233,13 @@ App({
   },
 
   // 请求封装
-  req(data,fn,htmlName,mts) {
+  req(data, fn, htmlName, mts) {
     // 判断token   刷新token 是否过期
-    var tokenStorage = wx.getStorageSync(this.globalData.saveTokenKey + this.globalData.openId)
-    if(tokenStorage){
-      var tokenTime = tokenStorage.token.time, refresh_tokenTime = tokenStorage.refresh_token.time;
-      wx.request({
-        url: this.data.timeUrl,
-        method: 'POST',
-        header: {
-          "content-type": 'application/x-www-form-urlencoded', // 默认值
-        },
-        success: res => {
-          if(tokenTime - res.data.second <50){
-            // token过期 
-            if(refresh_tokenTime - res.data.second < 50){
-              // 判断refresh_token是否过期
-              console.log('====??????更新token 过期了')
-              if (htmlName == 'index' || htmlName =='skuDetail'){
-                console.log('指定界面了走的游客token')
-                // 如果在首页则走游客token
-                this.touristToken()
-                this.nAjax(data, fn, mts)
-              }else{
-                console.log('没有指定界面 跳转登录界面')
-                wx.redirectTo({
-                  url: '/pages/login/login?url=/pages/index/index',
-                })
-              }
-            }else{
-              // 如果更新token没有过期则走更新token
-              console.log('token')
-              this.globalData.updateTokenData = tokenStorage.refresh_token.value;
-              this.upadteToken('req')
-              this.nAjax(data, fn, mts)
-            }
-          }else{
-            // token没有过期
-            console.log('====')
-            this.nAjax(data, fn, mts)
-          }
-        }
-      })
-    }else{
-      // 还没登录的情况下（走临时token）
-      this.nAjax(data, fn, mts)
-    }
+    this.globalData.tokenStorage = wx.getStorageSync(this.globalData.saveTokenKey + this.globalData.openId)
+    this.judgeToken();
+    this.nAjax(data, fn, mts)
   },
-  nAjax(data, fn, mts){
+  nAjax(data, fn, mts) {
     var json = {
       "content-type": 'application/json',
       "Authorization": this.globalData.token,
@@ -337,5 +274,5 @@ App({
       }
     })
   }
-  
+
 })
